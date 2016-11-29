a586799862ff19a85eabcf99e089dbdbbc397bb5
gallenhu <gallenhu@foxmail.com>
2016-11-29 18:13:33 +0800

    add component

---
+++ template/component/myNewComponent.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myNewComponent',
+  templateUrl: './myNewComponent.component.html',
+  styleUrls: ['./myNewComponent.component.css']
+})
+export class MyNewComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
